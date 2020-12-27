93a8fbf6de3841ca9644550171d4a512f06f4ed0
adam-chalmers <48976102+adam-chalmers@users.noreply.github.com>
2020-12-27 22:44:27 +1000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import wol from "wol"
+
+interface Host {
+    mac: string;
+    port: number;
+}
+
+export class WOL {
+    private readonly broadcastAddress: string;
+
+    constructor(broadcastAddress: string) {
+        this.broadcastAddress = broadcastAddress;
+    }
+
+    public async sendPacket(host: Host): Promise<boolean> {
+        try {
+            let result = await wol.wake(host.mac, { address: this.broadcastAddress, port: host.port });
+            if (result) {
+                return true;
+            } else {
+                throw new Error(`Failed to wake host using MAC ${host.mac} and port ${host.port}`)
+            }
+        } catch (err) {
+            let message = `Failed to wake host using MAC ${host.mac} and port ${host.port}`
+            if (err.message != null) {
+                message = `${message}\n${err.message}`
+            }
+            throw new Error(message);
+        }
+    }
+}
