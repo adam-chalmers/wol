e306f93e74569de9a3801cca6409a1d17ed22ea8
adam-chalmers <48976102+adam-chalmers@users.noreply.github.com>
2020-12-30 16:04:20 +1000

    Added more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,15 +3,30 @@ import { WOL } from "../src";
 import { Host } from "../src/types/host";
 
 describe("@adam-chalmers/wol @unit index.ts", () => {
-    const instance: WOL = new WOL("");
+    const broadcastAddress = "192.168.1.1";
+    const instance: WOL = new WOL(broadcastAddress);
     const host: Host = {
         mac: "01:23:45:67:89:AB",
         port: 9
     };
 
+    it("Should only call the wol lib once", async () => {
+        const wakeMock = jest.fn();
+        wol.wake = wakeMock.mockImplementation(async () => true);
+        await instance.sendPacket(host);
+        expect(wakeMock.mock.calls.length).toEqual(1);
+    });
+
+    it("Should call the wol lib with the given values", async () => {
+        const wakeMock = jest.fn();
+        wol.wake = wakeMock.mockImplementation(async () => true);
+        await instance.sendPacket(host);
+        expect(wakeMock.mock.calls[0]).toEqual([host.mac, { address: broadcastAddress, port: host.port }]);
+    });
+
     it("Should return true if the packet is successfully sent", () => {
         wol.wake = jest.fn().mockImplementation(async () => true);
-        expect(instance.sendPacket(host)).resolves.toBe(true);
+        expect(instance.sendPacket(host)).resolves.toEqual(true);
     });
 
     it("Should throw an error if the packet is not successfully sent", () => {
