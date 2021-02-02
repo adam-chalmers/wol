5fb8d3b0b6ffb67b24ab483cf6160d23bbdcee6e
adam-chalmers <48976102+adam-chalmers@users.noreply.github.com>
2021-02-02 20:51:28 +1000

    Removed unnecessary class
    Added export for Host interface

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,21 @@
 import wol from "wol";
 import { Host } from "./types/host";
 
-export class WOL {
-    private readonly broadcastAddress: string;
-
-    constructor(broadcastAddress: string) {
-        this.broadcastAddress = broadcastAddress;
-    }
-
-    public async sendPacket(host: Host): Promise<boolean> {
-        try {
-            const result = await wol.wake(host.mac, { address: this.broadcastAddress, port: host.port });
-            if (result) {
-                return true;
-            } else {
-                throw new Error(`Failed to wake host using MAC ${host.mac} and port ${host.port}`);
-            }
-        } catch (err) {
-            let message = `Failed to wake host using MAC ${host.mac} and port ${host.port}`;
-            if (err.message != null) {
-                message += `\n${err.message}`;
-            }
-            throw new Error(message);
+export async function wakeHost(host: Host, broadcastAddress: string): Promise<boolean> {
+    try {
+        const result = await wol.wake(host.mac, { address: broadcastAddress, port: host.port });
+        if (result) {
+            return true;
+        } else {
+            throw new Error(`Failed to wake host using MAC ${host.mac} and port ${host.port}`);
         }
+    } catch (err) {
+        let message = `Failed to wake host using MAC ${host.mac} and port ${host.port}`;
+        if (err.message != null) {
+            message += `\n${err.message}`;
+        }
+        throw new Error(message);
     }
 }
+
+export { Host } from "./types/host";
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,9 @@
 import wol from "wol";
-import { WOL } from "../src";
+import { wakeHost } from "../src";
 import { Host } from "../src/types/host";
 
 describe("@adam-chalmers/wol @unit index.ts", () => {
     const broadcastAddress = "192.168.1.1";
-    const instance: WOL = new WOL(broadcastAddress);
     const host: Host = {
         mac: "01:23:45:67:89:AB",
         port: 9
@@ -13,29 +12,29 @@ describe("@adam-chalmers/wol @unit index.ts", () => {
     it("Should only call the wol lib once", async () => {
         const wakeMock = jest.fn();
         wol.wake = wakeMock.mockImplementation(async () => true);
-        await instance.sendPacket(host);
+        await wakeHost(host, broadcastAddress);
         expect(wakeMock.mock.calls.length).toEqual(1);
     });
 
     it("Should call the wol lib with the given values", async () => {
         const wakeMock = jest.fn();
         wol.wake = wakeMock.mockImplementation(async () => true);
-        await instance.sendPacket(host);
+        await wakeHost(host, broadcastAddress);
         expect(wakeMock.mock.calls[0]).toEqual([host.mac, { address: broadcastAddress, port: host.port }]);
     });
 
     it("Should return true if the packet is successfully sent", async () => {
         wol.wake = jest.fn().mockImplementation(async () => true);
-        await expect(instance.sendPacket(host)).resolves.toEqual(true);
+        await expect(wakeHost(host, broadcastAddress)).resolves.toEqual(true);
     });
 
     it("Should throw an error if the packet is not successfully sent", async () => {
         wol.wake = jest.fn().mockImplementation(async () => false);
-        await expect(instance.sendPacket(host)).rejects.toThrowError();
+        await expect(wakeHost(host, broadcastAddress)).rejects.toThrowError();
     });
 
     it("Should throw an error if the wol lib throws an error", async () => {
         wol.wake = jest.fn().mockImplementation(() => Promise.reject(new Error("")));
-        await expect(instance.sendPacket(host)).rejects.toThrowError();
+        await expect(wakeHost(host, broadcastAddress)).rejects.toThrowError();
     });
 });
